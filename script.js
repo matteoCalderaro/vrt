2055eb061ea01e3843c0ba1bb288b9426756496f
Matteo Calderaro <matteo.calderaro@binetwork.it>
2024-03-13 13:42:37 +0100

    aggiunta pagina register

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 ////////////////////////////////////
 // PROGRESS BAR DESKTOP AND MOBILE
 
-let arr = ['Progetto','Dettagli ricerca','Beneficiario','Criterio 1','Criterio 2','Criterio 3','Criterio 4','Criterio 5','Criterio 6']
+var arrLogin = ['Progetto','Dettagli ricerca','Beneficiario','Criterio 1','Criterio 2','Criterio 3','Criterio 4','Criterio 5','Criterio 6']
+var arrRegister = ['Categoria','Anagrafica','Accettazioni','Sedi','Legale rappresentante','Dati specifici','Banca','Documenti']
 
 var currentStep = 1;
 var updateProgressBar;
 
-// progressMobile mobile - inizializza testo
-var progressMobile = arr[0]
+// passano la label alla progress mobile
+var arr
+var progressMobile 
+// definisce la lunghezza dello stepper
+var totalStepNumber
+
 
 
 function displayStep(stepNumber) {
-  // FORWARD ONLY ONE STEP
-  if(stepNumber > currentStep +1){
+  // avanti solo di uno step
+  if(stepNumber > currentStep +1 ){
     return
   }
-  // FORWARD & BACKWARD ONLY ONE STEP
+  // avanti e indietro solo uno step 
   // if(stepNumber > currentStep +1 || stepNumber < currentStep - 1){
   //     return
   // }
   
-  if (stepNumber >= 1 && stepNumber <= 9) {
+  if (stepNumber >= 1 && stepNumber <= totalStepNumber +1) {
     
     $('.circle').slice(1,stepNumber).removeClass('step-circle').addClass('step-circle-colored');
     
-    // FORWARD & BACKWARD ONLY ONE STEP
+    // avanti e indietro solo uno step 
     // if (stepNumber < currentStep) {
     //     $('.circle').eq(stepNumber).removeClass('step-circle-colored').addClass('step-circle')
     // }
@@ -45,24 +50,37 @@ function displayStep(stepNumber) {
   currentStep = stepNumber;
   
   // progressMobile - aggiorna testo
-  progressMobile=arr[currentStep-1]
+  progressMobile = arr[currentStep-1]
   
   updateProgressBar();
   $(window).scrollTop(0);
 }
 
 $(document).ready(function() {
+
+  let body =document.querySelector('body')
+  if(body.classList.contains('register')){
+    totalStepNumber = 7
+    arr = arrRegister
+  }
+  if(body.classList.contains('login')){
+    totalStepNumber = 8
+    arr = arrLogin
+  }
+  
+  console.log(arr)
+
   $('#multi-step-form').find('.step').slice(1).hide();
   
   
   $(".next-step").click(function() {
     
-    if (currentStep < 9) {
+    if (currentStep < totalStepNumber+1) {
       $('.progress-container').find('.circle').eq(currentStep).removeClass('step-circle').addClass('step-circle-colored')
       
       $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
       currentStep++;
-      // progressMobile - aggiorna testo
+      // progressMobile -> aggiorna testo
       progressMobile=arr[currentStep-1]
       setTimeout(function() {
         $(".step").removeClass("animate__animated animate__fadeOut").hide();
@@ -80,7 +98,7 @@ $(document).ready(function() {
       $('.progress-container').find('.circle').eq(currentStep-1).removeClass('step-circle-colored').addClass('step-circle')
       $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
       currentStep--;
-      // progressMobile - aggiorna testo
+      // progressMobile -> aggiorna testo
       progressMobile=arr[currentStep-1]
       setTimeout(function() {
         $(".step").removeClass("animate__animated animate__fadeOut").hide();
@@ -95,10 +113,11 @@ $(document).ready(function() {
   });
   
   updateProgressBar = function() {
-    var progressPercentage = ((currentStep - 1) / 8) * 100;
+    console.log(totalStepNumber)
+    var progressPercentage = ((currentStep - 1) / totalStepNumber) * 100;
     $(".progress-bar").css("width", progressPercentage + "%");
     
-    // progressMobile - aggiorna testo
+    // progressMobile -> aggiorna testo
     // $('.step-value').html(currentStep);
     $('.step-value').html(progressMobile);
   }

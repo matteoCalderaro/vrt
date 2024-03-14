e7f8fed2e4ada714acc3b3d80f2116509158d728
Matteo Calderaro <matteo.calderaro@binetwork.it>
2024-03-14 08:55:36 +0100

    inizio validazione step

--- script.js
+++ script.js
@@ -1,125 +1,156 @@
-////////////////////////////////////
-// PROGRESS BAR DESKTOP AND MOBILE
-
-var arrLogin = ['Progetto','Dettagli ricerca','Beneficiario','Criterio 1','Criterio 2','Criterio 3','Criterio 4','Criterio 5','Criterio 6']
-var arrRegister = ['Categoria','Anagrafica','Accettazioni','Sedi','Legale rappresentante','Dati specifici','Banca','Documenti']
+var labelArrayLogin = ['Progetto','Dettagli ricerca','Beneficiario','Criterio 1','Criterio 2','Criterio 3','Criterio 4','Criterio 5','Criterio 6']
+var labelArrayRegister = ['Categoria','Anagrafica','Accettazioni','Sedi','Legale rappresentante','Dati specifici','Banca','Documenti']
 
 var currentStep = 1;
 var updateProgressBar;
 
-// passano la label alla progress mobile
-var arr
-var progressMobile 
-// definisce la lunghezza dello stepper
+var labelArray
+var progressMobileLabel 
 var totalStepNumber
 
+// Intro:
+// Alla varibile globale currentStep è assegnato lo step corrente (le funzioni A + B aggiornano il valore)
+// Le funzioni A + B effettuano il toggle degli step (nodi dom). Il toggle è associato ad animazione css (libreria animate.css).
+// La funzione A è invocata da attributo onClick su labels e circonferenze (desktop). L'attributo passa il numero dello step verso il quale verrà effetuato il toggle.
+// La funzione B è invocata al caricamento dell'app e associa a tutti i bottoni avanti/indietro (desktop + mobile) un event handler
+// che effettua il toggle solo verso lo step precedente o successivo
 
 
+/* 
+  FUNCTION A
+*/
 function displayStep(stepNumber) {
-  // avanti solo di uno step
+
+  // esce se lo step cliccato non è quello immediatamente sucessivo a quello cliccato 
   if(stepNumber > currentStep +1 ){
     return
   }
-  // avanti e indietro solo uno step 
+  // esce se lo step cliccato non è quello immediatamente precedente o sucessivo a quello cliccato 
   // if(stepNumber > currentStep +1 || stepNumber < currentStep - 1){
   //     return
   // }
   
   if (stepNumber >= 1 && stepNumber <= totalStepNumber +1) {
-    
+
+    // colora le circonferenze dello step cliccato e dei precendenti 
     $('.circle').slice(1,stepNumber).removeClass('step-circle').addClass('step-circle-colored');
-    
-    // avanti e indietro solo uno step 
-    // if (stepNumber < currentStep) {
-    //     $('.circle').eq(stepNumber).removeClass('step-circle-colored').addClass('step-circle')
-    // }
-    
+
+    // decolora le circonferenze degli step seguenti se lo step cliccato è precedente al corrente
     if (stepNumber < currentStep) {
       $('.circle').slice(stepNumber).removeClass('step-circle-colored').addClass('step-circle')
     }
-    
+
+    // colora la label dello step cliccato
     $('.label').css("color", "var(--gray-light)").eq(stepNumber -1).css("color", "var(--secondary)")
-    // $(".step-" + currentStep).hide();
-    // $(".step-" + stepNumber).show();
+
+    // fade out corrente step
     $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
+
     setTimeout(function() {
+      // rimuove corrente step
       $(".step").removeClass("animate__animated animate__fadeOut").hide();
+      // visualizza step cliccato
       $(".step-" + stepNumber).show().addClass("animate__animated animate__fadeIn");
     }, 200);
   }
+  
   currentStep = stepNumber;
   
-  // progressMobile - aggiorna testo
-  progressMobile = arr[currentStep-1]
-  
+  // aggiorna la label (mobile)
+  progressMobileLabel = labelArray[currentStep-1]
+
   updateProgressBar();
   $(window).scrollTop(0);
 }
 
+/* 
+  FUNCTION B effettua inoltre:
+    -  definisce il totale degli step in base alla pagina (register/login)
+    -  inizializza la funzione updateProgressBar che definisce la lunghezza della progress bar desktop/mobile)
+*/
 $(document).ready(function() {
 
+  // elimina dal dom tutti gli step, tranne lo step 1
+  $('.form-container').find('.step').slice(1).hide();
+
+  // totale step in base alle pagina
   let body =document.querySelector('body')
   if(body.classList.contains('register')){
     totalStepNumber = 7
-    arr = arrRegister
+    labelArray = labelArrayRegister
   }
   if(body.classList.contains('login')){
     totalStepNumber = 8
-    arr = arrLogin
+    labelArray = labelArrayLogin
   }
   
-  console.log(arr)
-
-  $('#multi-step-form').find('.step').slice(1).hide();
   
-  
+  // bottone avanti event handler
   $(".next-step").click(function() {
-    
     if (currentStep < totalStepNumber+1) {
-      $('.progress-container').find('.circle').eq(currentStep).removeClass('step-circle').addClass('step-circle-colored')
       
+      // CHIAMA FUNZIONI VALIDAZIONE INPUT
+      eval(`validateStep${currentStep}`)();
+     
+      // colora la circonferenza dello step cliccato 
+      $('.circle').eq(currentStep).removeClass('step-circle').addClass('step-circle-colored')
+      // colora la label cliccata
+      $('.label').css("color", "var(--gray-light)").eq(currentStep).css("color", "var(--secondary)");
+      // fade out corrente step
       $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
+      
       currentStep++;
-      // progressMobile -> aggiorna testo
-      progressMobile=arr[currentStep-1]
+      
+      // mobile | trova la label nel rispettivo array
+      progressMobileLabel = labelArray[currentStep-1]
+
       setTimeout(function() {
+        // rimuove corrente step
         $(".step").removeClass("animate__animated animate__fadeOut").hide();
+        // visualizza sucessivo step
         $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
         updateProgressBar();
       }, 200);
     }
-    $('.label').css("color", "var(--gray-light)").eq(currentStep -1).css("color", "var(--secondary)");
-    $('.circle').slice(1,currentStep).removeClass('step-circle').addClass('step-circle-colored');
+
     $(window).scrollTop(0);
   });
   
+
+  // bottone indietro event handler
   $(".prev-step").click(function() {
+
     if (currentStep > 1) {
-      $('.progress-container').find('.circle').eq(currentStep-1).removeClass('step-circle-colored').addClass('step-circle')
+      // decolora la circonferenza del corrente step
+      $('.circle').eq(currentStep-1).removeClass('step-circle-colored').addClass('step-circle')
+      // colora la label del precedende step
+      $('.label').css("color", "var(--gray-light)").eq(currentStep -2).css("color", "var(--secondary)");
+      // fade out corrente step
       $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
       currentStep--;
-      // progressMobile -> aggiorna testo
-      progressMobile=arr[currentStep-1]
+      
+      progressMobileLabel = labelArray[currentStep-1]
+
       setTimeout(function() {
+        // rimuove corrente step
         $(".step").removeClass("animate__animated animate__fadeOut").hide();
+        // visualizza precedente step
         $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
         updateProgressBar();
       }, 200);
     }
-    $('.label').css("color", "var(--gray-light)").eq(currentStep -1).css("color", "var(--secondary)");
-    $('.circle').slice(currentStep).removeClass('step-circle-colored').addClass('step-circle');
+
     $(window).scrollTop(0);
-    
   });
   
+  // definisce la lunghezza delle progress bar 
   updateProgressBar = function() {
-    console.log(totalStepNumber)
+    // desktop + mobile | definisce la lunghezza della barra
     var progressPercentage = ((currentStep - 1) / totalStepNumber) * 100;
     $(".progress-bar").css("width", progressPercentage + "%");
     
-    // progressMobile -> aggiorna testo
-    // $('.step-value').html(currentStep);
-    $('.step-value').html(progressMobile);
+    // mobile | assegna la stringa alla label
+    $('.step-value').html(progressMobileLabel);
   }
 });
 
@@ -140,7 +171,6 @@ window.addEventListener("scroll", () => {
     scrollIcon.style.visibility = "visible"
   }
   else {
-    
     scrollIcon.style.visibility = "hidden"
   }
 });
